c7d7d7060ff26cba47828995d239076bdbcfc278
Matteo Franchini <matteo.franchini@intre.it>
2021-10-22 15:35:06 +0200

    [FEAT] Added opening of add.html

--- main.js
+++ main.js
@@ -7,7 +7,11 @@ function createWindow () {
     const mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+
         webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             preload: path.join(__dirname, 'preload.js')
         }
     })
