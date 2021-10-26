d93f0bf2c439a6b3d1ea9ea4d2f243f3b9492a9b
Matteo Franchini <matteo.franchini@intre.it>
2021-10-26 15:48:15 +0200

    [ADD-ALARM][FIx] changed close listener button

--- src/addAlarm/addAlarmRender.js
+++ src/addAlarm/addAlarmRender.js
@@ -3,12 +3,16 @@ const {BrowserWindow}= require('electron')
 const thisWin = ()=>{ console.log(BrowserWindow.getFocusedWindow()); return BrowserWindow.getFocusedWindow()}
 
 //region behaviours
-//region button behaviours
+//region close window
+
+document.addEventListener('', function(){
+
+})
 /**
  * here we say that onClick of the retrieved button we want to go to the add page
  * @type {HTMLElement}
  */
-const closeBtn = document.getElementById("updateBtn")
+const closeBtn = document.getElementById("closeBtn")
 closeBtn.addEventListener('click',function(){
     console.log("aasasa",thisWin)
     thisWin.close()
