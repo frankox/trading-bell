b501ba9a5539eff8de6a34705dd665b686b46a82
Matteo Franchini <matteo.franchini@intre.it>
2021-10-26 16:59:44 +0200

    [Check-prices][FEAT] created class to contain personal api keys

--- src/indexPageRenderer.js
+++ src/indexPageRenderer.js
@@ -1,7 +1,35 @@
+import {CRYPTOCOMPARE_READONLY_APIKEY} from '/src/utils/configs.local'
 const path = require('path')
+const axios = require("axios")
 
 //region behaviours
-//region open AddAlarm window
+
+//region price
+//region html components retrieval
+const price = document.getElementById('price')
+const targetPrice = document.getElementById('target-price')
+//endregion
+
+//region getCurrentPrice
+//region mainFunction
+function getCurrentPrice(){
+    axios.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH&tsyms=BTC,USD,EUR&api_key='+CRYPTOCOMPARE_READONLY_APIKEY)
+        .then(response =>{
+            const cryptos = response.data
+            console.log(cryptos)
+        })
+}
+//endregion
+getCurrentPrice()
+setInterval(getCurrentPrice, 30000) //every 30 secs
+//region scheduler
+
+//endregion
+
+//endregion
+//endregion
+
+//region closeBtn
 /**
  * here we say that onClick of the retrieved button we want to go to the add page
  * @type {HTMLElement}
