1b13e58cc054588acc37568539ffca35459714fe
matteo franchini <matteo.franchini@xeos.it>
2021-10-30 14:22:20 +0200

    [FIX] notification now triggers only if current state changed

--- src/indexPageRenderer.js
+++ src/indexPageRenderer.js
@@ -1,7 +1,7 @@
 const axios = require("axios")
 
 //region enums
-const targetStates = {
+const TargetStates = {
     ABOVE: "above",
     BELOW: "below",
     NONE: "none"
@@ -9,7 +9,7 @@ const targetStates = {
 const tokens = {
     TIME: "TIME",
     OHM: "OHM",
-    KLIMA: "klima"
+    KLIMA: "KLIMA"
 }
 //endregion
 
@@ -18,20 +18,23 @@ const targetPrice = document.getElementById('target-price')
 const targetPriceInput = document.getElementById("target-price-input")
 const updateAlarmBtn = document.getElementById("update-alarm-update-btn")
 const targetValue = ()=>{ return Number(targetPriceInput.value)}
+let currentTarget = targetValue()
 updateAlarmBtn.addEventListener('click',function() {
-    targetPrice.innerText = 'Current Target: $'+targetValue().toLocaleString('en')
+    targetPrice.innerText = 'Current Target: €'+targetValue().toLocaleString('en')
+    currentTarget = targetValue()
 })
 //endregion
 
 //region current price
 const price = document.getElementById('price')
 //region notification
+let previousState = TargetStates.NONE
 const currentTargetState = (price, target) =>{
     return target === 0 ?
-        targetStates.NONE :
-        target > price ?
-            targetStates.ABOVE :
-            targetStates.BELOW
+        TargetStates.NONE :
+        target < price ?
+            TargetStates.ABOVE :
+            TargetStates.BELOW
 }
 
 const notificationText = (token, currentTargetState) =>{
@@ -46,25 +49,25 @@ function creatNotification(tokenName, currentTargetState){
 }
 
 //endregion
-
-
-function getCurrentPrice() {
+function getCurrentPriceAndSendNotification() {
     axios.get('https://min-api.cryptocompare.com/data/price?fsym=OHM&tsyms=EUR')
         .then(response => {
             const cryptos = response.data
             price.innerText = cryptos.EUR
         })
 
+    console.log(targetValue())
     const currentState = currentTargetState(price.innerText, targetValue())
-    if (currentState && currentState !== targetStates.NONE) {
+    if (currentState && currentState !== previousState) {
         creatNotification(tokens.OHM, currentState)
+        previousState = currentState
     }
 }
 //endregion
 
 //region scheduler
-getCurrentPrice()
-setInterval(getCurrentPrice, 30000) //every 30 secs
+getCurrentPriceAndSendNotification()
+setInterval(getCurrentPriceAndSendNotification, 3000) //every 30 secs
 //endregion
 
 
